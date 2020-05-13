91013a5c47bc77fb723851ceda74cb5aae06dfef
Magico <magicocarella@gmail.com>
2020-05-13 02:23:03 +0100

    Quinto

--- server/config/config.js
+++ server/config/config.js
@@ -19,6 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = process.env.MONGO_URI;
+    //urlDB = process.env.MONGO_URI;
+    urlDB = 'mongodb://admin:p4G4vDle5XjoRrTu@cluster0-ftdpr.mongodb.net/cafe';
 }
 process.env.URLDB = urlDB;
